ccf90a9bc743033850bd6bbd6fa7ea608ea82e1e
makyia bonner <makyiabonner@gmail.com>
2022-10-17 18:20:13 -0400

    sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(startArr, endArr) {
+if (startArr < 0 || endArr < 0) return "ERROR";
+if (!Number.isInteger(startArr) || !Number.isInteger(endArr)) return "ERROR";
+    let sum = 0;
+for(i = startArr; i <= endArr; i++){
+    sum+= i;
+}
+return sum;
 };
 
 // Do not edit below this line
