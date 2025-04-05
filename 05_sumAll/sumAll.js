5d713c0452ed3e552e9e1b050f94b20599b81074
makyia bonner <makyiabonner@gmail.com>
2025-04-05 13:36:09 -0400

    fixed functionality for sumAll and also passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1 = 0, num2 = 0) {
+    if(
+        !Number.isInteger(num1) || 
+        !Number.isInteger(num2) || 
+        typeof num1 !== 'number' ||
+        typeof num2 !== 'number' ||
+        num1 < 0 ||
+        num2 < 0){
+        return 'ERROR'
+    }
+    else{
+        let sum = 0;
+        const largestNum = Math.max(num1,num2);
+        const smallestNum = Math.min(num1,num2);
 
+        for(let int = smallestNum; int <= largestNum; int++){
+            sum += int;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
