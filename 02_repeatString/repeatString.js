e42ec6d2e39a537430d5fd1cc4876b6112940fb1
makyia bonner <makyiabonner@gmail.com>
2025-04-05 11:56:12 -0400

    repeatString has functionality and passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) return "ERROR";
+  else {
+    const arr = [];
+    for (let i = 0; i < num; i++) {
+      arr.push(str);
+    }
+    const repeatedString = arr.join("");
+    return repeatedString;
+  }
 };
 
 // Do not edit below this line
